833186c8aff1d61d6c267f76c52279ea1f5a3e2f
seungmin <ssmmkim1025@naver.com>
2026-07-06 03:59:47 +0900

    ✨ Feat: 라우팅 및 공통 웹앱 레이아웃 구성

---
+++ src/layout/RootLayout.jsx
@@ -0,0 +1,29 @@
+import {Outlet} from "react-router-dom"
+import Header from "../components/header/Header"
+import Footer from "../components/footer/Footer"
+import styled from "styled-components"
+
+export default function RootLayout(){
+    return(
+        <AppLayout>
+            <Header />
+            <main className="app-content">
+                <Outlet />
+            </main>
+            <Footer />
+        </AppLayout>
+    );
+}
+const AppLayout = styled.div`
+    width: 100%;
+    max-width: 390px;
+    min-height: 844dvh;
+    margin: 0 auto;
+    padding : 30px;
+
+    display: flex;
+    flex-direction: column;
+
+    position: relative;
+    background-color: #ffffff;
+`;
